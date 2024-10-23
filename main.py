e485d877ab5224e505fb9008c5cb6649d85cf900
Borislav <borislav.tsvetkov02@gmail.com>
2024-10-23 19:53:52 +0300

    refactor: remove unnecessary comments

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 import os
 from discord.ext import commands
-from parse import (
+from unit_stats_query import (
     query_unit_stats
 )
 import logging
@@ -24,21 +24,21 @@ bot_token = os.getenv("BOT_TOKEN")
 intents = discord.Intents.default()
 intents.messages = True
 intents.message_content = True
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix = "!", intents=intents)
 
 @bot.command(name='unit_stats')
 async def unit_stats(ctx, *, unit_name: str):  # Capture the entire unit name
     logging.info(f"Received unit name: '{unit_name}'")
-    unit_info = query_unit_stats(unit_name)  # Get the unit info
+    unit_info = query_unit_stats(unit_name)
     if unit_info == "Unit not found":
-        await ctx.send(unit_info)  # Send the error message if unit not found
+        await ctx.send(unit_info)
     else:
         # Format the response with bullet points, excluding 'Unit', 'Soldiers', and 'Campaign Cost'
         response = f"Information for {unit_name}:\n```"
         for key, value in unit_info.items():
-            if key not in ['Unit', 'Soldiers', 'Campaign Cost']:  # Exclude specified keys
+            if key not in ['Unit', 'Soldiers', 'Campaign Cost']:
                 response += f"• {key}: {value}\n"
         response += "```"
-        await ctx.send(response)  # Send the formatted response
+        await ctx.send(response)
 
 bot.run(bot_token)

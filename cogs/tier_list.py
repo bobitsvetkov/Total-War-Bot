61c5b147872accfbfa7c6a69869a5f4c516922c8
Borislav <borislav.tsvetkov02@gmail.com>
2024-10-25 18:28:24 +0300

    fix: fixed the path for the tier list image

--- cogs/tier_list.py
+++ cogs/tier_list.py
@@ -8,7 +8,7 @@ class TierListCog(commands.Cog):
 
     @commands.command(name='tier_list', help='Send the tier list image.')
     async def send_tierlist(self, ctx):
-        image_path = os.path.join(os.path.dirname(__file__), 'images', '3v3 Tier List.png')
+        image_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', '3v3 Tier List.png')
         if os.path.exists(image_path):
             await ctx.send(file=discord.File(image_path))
         else:
